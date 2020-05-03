3ff204de5207a05485c59c86527ae5b38e1ff5ae
Johann Pardanaud <pardanaud.j@gmail.com>
2020-05-03 17:47:38 +0200

    Improve the blog  page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link, graphql } from 'gatsby'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
 import { Layout } from '../components/layout'
 import { Content } from '../components/content'
 import { SEO } from '../components/seo'
 
-const Post = styled.article`
-    margin-bottom: calc(var(--content-spacing) * 2);
+const ReadMore = styled.span`
+    color: var(--primary-500);
 
+    svg {
+        margin-left: calc(var(--content-spacing) / 2);
+    }
+`
+
+const PostContainer = styled.div`
+    &:not(:last-child) {
+        margin-bottom: calc(var(--content-spacing) * 3);
+    }
+
+    a,
+    a:visited,
+    a:hover,
+    a:focus,
+    a:active {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    a:hover ${ReadMore}, a:focus ${ReadMore}, a:active ${ReadMore} {
+        color: var(--primary-700);
+        text-decoration: underline;
+    }
+`
+
+const Post = styled.article`
     h2 {
         font-size: 2rem;
         letter-spacing: 0.02rem;
@@ -20,14 +48,20 @@ export default ({ data }) => {
         <Layout seo={<SEO title="Blog" />}>
             <Content>
                 {posts.map((post) => (
-                    <Post>
-                        <h2>
-                            <Link to={`/blog/${post.frontmatter.slug}`}>
-                                {post.frontmatter.title}
-                            </Link>
-                        </h2>
-                        <p>{post.excerpt}</p>
-                    </Post>
+                    <PostContainer key={post.frontmatter.slug}>
+                        <Link to={`/blog/${post.frontmatter.slug}`}>
+                            <Post>
+                                <h2>{post.frontmatter.title}</h2>
+                                <p>{post.excerpt}</p>
+                                <ReadMore>
+                                    Read more
+                                    <FontAwesomeIcon
+                                        icon={faLongArrowAltRight}
+                                    />
+                                </ReadMore>
+                            </Post>
+                        </Link>
+                    </PostContainer>
                 ))}
             </Content>
         </Layout>
