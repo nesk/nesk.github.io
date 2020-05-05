28ce06d356fb52d2712a14f174e295ef17579eba
Johann Pardanaud <pardanaud.j@gmail.com>
2020-05-05 14:30:55 +0200

    Remove useless gatsby-plugin-react-svg package

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,13 +47,5 @@ module.exports = {
         'gatsby-transformer-sharp',
         'gatsby-plugin-sharp',
         'gatsby-plugin-styled-components',
-        {
-            resolve: 'gatsby-plugin-react-svg',
-            options: {
-                rule: {
-                    include: /icons/,
-                },
-            },
-        },
     ],
 }
