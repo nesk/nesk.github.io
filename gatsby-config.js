87ed82b7568e8dc2516e9e8df09132e1a3ee5e62
Johann Pardanaud <pardanaud.j@gmail.com>
2020-11-08 22:22:22 +0100

    Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,7 @@ module.exports = {
         'gatsby-transformer-sharp',
         'gatsby-plugin-sharp',
         'gatsby-plugin-styled-components',
+        'gatsby-plugin-sitemap',
         {
             resolve: `gatsby-plugin-feed`,
             options: {
